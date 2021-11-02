4347cc6b829b8bf613acb4ccf82a5bd463c198ab
Chapelle Nathan <nathanchapelle76@gmail.com>
2021-11-02 11:19:04 +0100

    commit

--- index.php
+++ index.php
@@ -4,5 +4,7 @@ $destinationTime = new DateTime('yesterday 01:00');
 echo nl2br($presentTime->format('m/d/Y H:i') . PHP_EOL);
 echo nl2br($destinationTime->format('m/d/Y H:i') . PHP_EOL);
 $diff = $destinationTime->diff($presentTime, true);
-echo 'Il y  a ' . $diff->m . ' mois, ' . $diff->d . ' jours, ' . $diff->y . ' années et ' . $diff->h . ' heures, ' . $diff->i . 
-' secondes de différences par rapport a la fin du Zevent :\'(';
+echo nl2br('Il y  a ' . $diff->m . ' mois, ' . $diff->d . ' jours, ' . $diff->y . ' années et ' . $diff->h . ' heures, ' . $diff->i . 
+' secondes de différences par rapport a la fin du Zevent :\'(' . PHP_EOL);
+$minutes = $diff->days * 24 * 60;
+echo 'On a besoins de ' . floor($minutes / 1000) . ' litre(s) de carburant' . PHP_EOL;
