fa026be5c01a8f29a8e79bb7a6fbf3bfff58151a
Chapelle Nathan <nathanchapelle76@gmail.com>
2021-11-02 10:22:50 +0100

    fixed issues

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $presentTime = new DateTime('NOW');
-$destinationTime = new DateTime('NOW', new DateTimeZone('America/Toronto'));
-$diff = $destinationTime->diff($presentTime, true);
+$destinationTime = new DateTime('yesterday 01:00');
 echo nl2br($presentTime->format('m/d/Y H:i') . PHP_EOL);
 echo nl2br($destinationTime->format('m/d/Y H:i') . PHP_EOL);
-var_dump($diff);
-echo 'Il y  a ' . $diff->m . ' mois, ' . $diff->d . ' années, ' . $diff->y . ' et ' . $diff->h . ' heures, ' . $diff->i . ' secondes de différences';
+$diff = $destinationTime->diff($presentTime, true);
+echo 'Il y  a ' . $diff->m . ' mois, ' . $diff->d . ' jours, ' . $diff->y . ' années et ' . $diff->h . ' heures, ' . $diff->i . 
+' secondes de différences par rapport a la fin du Zevent :\'(';
